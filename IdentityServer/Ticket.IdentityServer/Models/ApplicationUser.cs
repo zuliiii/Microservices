8cd492a3ae9918c8a2a5a2cd8a837077f0209bc8
zuliiii <zuleyxasuleymanova@gmail.com>
2023-07-20 15:00:27 +0400

    IdentityServer added

---
+++ IdentityServer/Ticket.IdentityServer/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Ticket.IdentityServer.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
