e8370fd0590f075e5f71dba6f312720da5292430
zuliiii <zuleyxasuleymanova@gmail.com>
2023-07-24 16:54:38 +0400

    PhotoStock Controller created

---
+++ Services/PhotoStock/Ticket.Services.PhotoStock/Controllers/PhotosController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Ticket.Services.PhotoStock.DTOs;
+using Ticket.Shared.ControllerBases;
+using Ticket.Shared.DTOs;
+
+namespace Ticket.Services.PhotoStock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotosController : CustomBaseController
+    {
+        public async Task<IActionResult> PhotoSave(IFormFile photo, CancellationToken cancellationToken)
+        {
+            if(photo !=null && photo.Length > 0)
+            {
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photo.FileName);
+
+                using(var stream = new FileStream(path,FileMode.Create))
+                {
+                    await photo.CopyToAsync(stream, cancellationToken);
+                }
+
+                var returnPath = "photos/" + photo.FileName;
+
+                PhotoDto photoDto = new() { Url = returnPath };
+
+                return CreateActionResultInstance(Response<PhotoDto>.Success(photoDto, StatusCodes.Status200OK));
+            }
+
+            return CreateActionResultInstance(Response<PhotoDto>.Fail("photo is empty", StatusCodes.Status400BadRequest));
+        }
+    }
+}
---
+++ Services/PhotoStock/Ticket.Services.PhotoStock/DTOs/PhotoDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Ticket.Services.PhotoStock.DTOs
+{
+    public class PhotoDto
+    {
+        public string Url { get; set; }
+    }
+}
