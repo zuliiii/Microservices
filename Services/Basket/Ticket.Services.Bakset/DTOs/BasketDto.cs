a53047faf3cea28abae68b84ea185581cb005878
zuliiii <zuleyxasuleymanova@gmail.com>
2023-07-27 10:36:40 +0400

    BasketItemDto and BasketItem added

---
+++ Services/Basket/Ticket.Services.Bakset/DTOs/BasketDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Ticket.Services.Bakset.DTOs
+{
+    public class BasketDto
+    {
+        public string UserId { get; set; }
+        public string DiscountCode { get; set; }
+
+        public List<BasketItemDto> basketItems { get; set; }
+
+        public decimal TotalPrice {
+            get => basketItems.Sum(x => x.Quantity * x.Price);
+                }
+    }
+}
---
+++ Services/Basket/Ticket.Services.Bakset/DTOs/BasketItemDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Ticket.Services.Bakset.DTOs
+{
+    public class BasketItemDto
+    {
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public string EventId { get; set;}
+        public string EventTitle { get; set;}
+    }
+}
