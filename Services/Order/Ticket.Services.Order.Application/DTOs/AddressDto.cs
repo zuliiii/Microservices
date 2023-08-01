b1d98cd3d80d2880b102bc7b0ddbaa24f98b7256
zuliiii <zuleyxasuleymanova@gmail.com>
2023-08-01 23:51:18 +0400

    DTOs created

---
+++ Services/Order/Ticket.Services.Order.Application/DTOs/AddressDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticket.Services.Order.Application.DTOs
+{
+    public class AddressDto
+    {
+        public string Country { get; private set; }
+        public string State { get; private set; }
+        public string City { get; private set; }
+        public string ZipCode { get; private set; }
+    }
+}
---
+++ Services/Order/Ticket.Services.Order.Application/DTOs/OrderItemDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticket.Services.Order.Application.DTOs
+{
+    public class OrderItemDto
+    {
+        public string ProductId { get; private set; }
+        public string ProductName { get; private set; }
+        public string PictureUrl { get; private set; }
+        public decimal Price { get; private set; }
+        public int Quantity { get; private set; }
+        public decimal Total { get => Price * Quantity; }
+    }
+}
