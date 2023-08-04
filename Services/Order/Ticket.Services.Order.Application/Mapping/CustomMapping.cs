c3829e1adac1006cff3697cfe0500276b6630210
zuliiii <zuleyxasuleymanova@gmail.com>
2023-08-04 18:05:25 +0400

    AutoMapper added

---
+++ Services/Order/Ticket.Services.Order.Application/Mapping/CustomMapping.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ticket.Services.Order.Application.DTOs;
+using Ticket.Services.Order.Domain.OrderAggregate;
+
+namespace Ticket.Services.Order.Application.Mapping
+{
+    public class CustomMapping:Profile
+    {
+        public CustomMapping()
+        {
+            CreateMap<Domain.OrderAggregate.Order, OrderDto>().ReverseMap();
+            CreateMap<OrderItem, OrderItemDto>().ReverseMap();
+            CreateMap<Address, AddressDto>().ReverseMap();
+        }
+    }
+}
