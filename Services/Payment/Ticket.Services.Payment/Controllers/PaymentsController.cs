9645492a020509b92dd55d05e0ff91784ebe6207
zuliiii <zuleyxasuleymanova@gmail.com>
2023-08-05 16:18:29 +0400

    Paymentcontroller added

---
+++ Services/Payment/Ticket.Services.Payment/Controllers/PaymentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Ticket.Shared.ControllerBases;
+using Ticket.Shared.DTOs;
+
+namespace Ticket.Services.Payment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentsController : CustomBaseController
+    {
+        [HttpPost]
+        public IActionResult ReceivePayment()
+        {
+            return CreateActionResultInstance(Response<NoContent>.Success(StatusCodes.Status200OK));
+        }
+    }
+}
