4aeb82c61e6241e082bef38f546a0be9e6552395
zuliiii <zuleyxasuleymanova@gmail.com>
2023-08-06 11:43:21 +0400

    Models created

---
+++ Client/Ticket.Web/Models/ServiceApiSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Ticket.Web.Models
+{
+    public class ServiceApiSettings
+    {
+        public string BaseUri { get; set; }
+        public string PhotoStockUri { get; set; }
+    }
+}
---
+++ Client/Ticket.Web/Models/SigninInput.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Ticket.Web.Models
+{
+    public class SigninInput
+    {
+        [Required]
+        [Display(Name = "Enter your email")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Enter your password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me")]
+        public bool IsRemember { get; set; }
+    }
+}
