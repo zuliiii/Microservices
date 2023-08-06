a28479bae4fa61bdfdbd92fabb5d896a25c97559
zuliiii <zuleyxasuleymanova@gmail.com>
2023-08-06 11:43:41 +0400

    IIdentityService interface added

---
+++ Client/Ticket.Web/Services/Interfaces/IIdentityService.cs
@@ -0,0 +1,15 @@
+﻿using IdentityModel.Client;
+using Ticket.Shared.DTOs;
+using Ticket.Web.Models;
+
+namespace Ticket.Web.Services.Interfaces
+{
+    public interface IIdentityService
+    {
+        Task<Response<bool>> SignIn(SigninInput signinInput);
+
+        Task<TokenResponse> GetAccessTokenByRefreshToken();
+
+        Task RevokeRefreshToken();
+    }
+}
