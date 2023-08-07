60253ddfc4048d778fd6f715a74e2c1e7aab61d0
zuliiii <zuleyxasuleymanova@gmail.com>
2023-08-07 13:03:15 +0400

    AuthControllder added

---
+++ Client/Ticket.Web/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ticket.Web.Models;
+
+namespace Ticket.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+		public async Task<IActionResult> SignIn(SigninInput signinInput)
+		{
+			return View();
+		}
+	}
+}
