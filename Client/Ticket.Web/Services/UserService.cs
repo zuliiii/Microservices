910d8c2bd7609c1e98fab3ba4f86137404c3ec70
zuliiii <zuleyxasuleymanova@gmail.com>
2023-08-08 15:59:50 +0400

    UserService added

---
+++ Client/Ticket.Web/Services/UserService.cs
@@ -0,0 +1,13 @@
+﻿using Ticket.Web.Models;
+using Ticket.Web.Services.Interfaces;
+
+namespace Ticket.Web.Services
+{
+	public class UserService : IUserService
+	{
+		public Task<UserViewModel> GetUser()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
