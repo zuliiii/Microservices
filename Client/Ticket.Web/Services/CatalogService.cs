d084e79778275f7e8f55475477b36ead04536ba2
zuliiii <zuleyxasuleymanova@gmail.com>
2023-08-09 15:35:04 +0400

    CatalogService added

---
+++ Client/Ticket.Web/Services/CatalogService.cs
@@ -0,0 +1,45 @@
+﻿using Ticket.Web.Models.Catalog;
+using Ticket.Web.Services.Interfaces;
+
+namespace Ticket.Web.Services
+{
+	public class CatalogService : ICatalogService
+	{
+		private readonly HttpClient _httpClient;
+
+		public CatalogService(HttpClient httpClient)
+		{
+			_httpClient = httpClient;
+		}
+
+		public Task<bool> CreateEventAsync(EventCreateInput eventCreateInput)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<bool> DeleteEventAsync(string eventId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<List<CategoryViewModel>> GetAllCategoryAsync()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<List<EventViewModel>> GetAllEventAsync()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<EventViewModel> GetByEventId(string eventId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<bool> UpdateEventAsync(EventCreateInput eventUpdateInput)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
