7dbfcf28d81294645c8c47a587f44400f8f5fec1
zuliiii <zuleyxasuleymanova@gmail.com>
2023-08-11 18:39:23 +0400

    PhotosHelper added

---
+++ Client/Ticket.Web/Helpers/PhotoHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Options;
+using Ticket.Web.Models;
+
+namespace Ticket.Web.Helpers
+{
+	public class PhotoHelper
+	{
+		private readonly ServiceApiSettings _serviceApiSettings;
+
+		public PhotoHelper(IOptions<ServiceApiSettings> serviceApiSettings)
+		{
+			_serviceApiSettings = serviceApiSettings.Value;
+		}
+
+		public string GetPhotoStockUrl(string photoUrl)
+		{
+			return $"{_serviceApiSettings.PhotoStockUri}/photos/{photoUrl}";
+		}
+	}
+}
